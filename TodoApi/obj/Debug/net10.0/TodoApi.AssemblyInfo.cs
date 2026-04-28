858cc5dc52bbca9ea5756d7d3e1237f9dfced9f2
PHATPHIMONs <phatphimon2244@gmail.com>
2026-04-28 16:22:09 +0700

    build: update binaries and cache files for improved performance and debugging

--- TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a593d05d8e2baa9a4e4bf8943316adbaa260fa6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b4e029ef1fcafdb60068f1481db3b9de00707b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
